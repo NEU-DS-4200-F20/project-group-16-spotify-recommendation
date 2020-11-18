0c7a5a79829f2e6cd49b83c4d440171148d88143
John Barber <johnwbarber@icloud.com>
2020-11-18 00:59:37 -0500

    Fixed table scrolling

--- js/table.js
+++ js/table.js
@@ -46,24 +46,24 @@ function table() {
     startPos = 0,
     increment = 10;
 
-  var scrollEvent = d3.zoom()
-    .on('zoom', function (e) {
-      var scrollDirection = (d3.event.deltaY > 0) ? 1 : -1;
-      startPos += (scrollDirection > 0 && startPos + increment < allData.length) ? scrollDirection
-        : (scrollDirection < 0 && startPos + increment > increment) ? scrollDirection
-          : 0;
-      updateTable();
-    });
+  // var scrollEvent = d3.zoom()
+  //   .on('zoom', function (e) {
+  //     var scrollDirection = (d3.event.deltaY > 0) ? 1 : -1;
+  //     startPos += (scrollDirection > 0 && startPos + increment < allData.length) ? scrollDirection
+  //       : (scrollDirection < 0 && startPos + increment > increment) ? scrollDirection
+  //         : 0;
+  //     updateTable();
+  //   });
 
   //sets table properties
   let columns = ['track_name', 'artist_name', 'album_name']
   let columnNames = ['Title', 'Artist', 'Album']
   let table = d3.select('#table')
     // .attr("style", "margin-center")
-    .style("width", "25%")
+    //.style("width", "25%")
   // .style("border", "1px black solid")
   let thead = table.append('thead')
-  let tbody = table.append('tbody').call(scrollEvent);
+  let tbody = table.append('tbody')//.call(scrollEvent);
 
 
   d3.csv("/data/john_liked_songs.csv").then(function (data) {
@@ -83,66 +83,66 @@ function table() {
     .style("border", "1px lightgrey solid")
     .text(function (column) { return column; });
 
-    updateTable();
+    //updateTable();
 
-    // creates the rows for tables
-    // var rows = tbody.selectAll('tr')//.append('#svg')
-    //   .data(data)
-    //   .enter()
-    //   .append('tr');
+    //creates the rows for tables
+    var rows = tbody.selectAll('tr')//.append('#svg')
+      .data(data)
+      .enter()
+      .append('tr');
 
-    // var cells = rows.selectAll('td')//.append('#svg')
-    // .data(function (row) {
-    //   return columns.map(function (column) {
-    //     return { column: column, value: row[column] };
-    //   });
-    // })
-    //   //const path = svg.selectAll('path')
-    //   .enter()
-    //   .append('td')
-    //   //.append('path')
-    //   .text(function (d) { return d.value; })
-    //   .style("border-collapse", "collapse")
-    //   .style("padding", "6px")
-    //   .style("border", "1px lightgrey solid");
-
-  });
-
-  function updateTable() {
-    // Set new data based on startPos and increment.
-    currentData = allData.slice(startPos, startPos + increment);
-
-    // Delete previous rows.
-    tbody.selectAll('tr').remove();
-
-    // Create new rows.
-    var tr = tbody.selectAll("tr")
-      .data(currentData).enter()
-      .append("tr")
-      .classed("even", function (d, i) {
-        return i % 2 == 1;
+    var cells = rows.selectAll('td')//.append('#svg')
+    .data(function (row) {
+      return columns.map(function (column) {
+        return { column: column, value: row[column] };
       });
-
-    tr.selectAll('td')
-      .data(function (row) {
-        return columns.map(function (column) {
-          return { column: column, value: row[column] };
-        });
-      })
-      // .data(function (d) {
-      //   return keys.map(function (e) {
-      //     return {
-      //       key: e,
-      //       value: d[e]
-      //     }
-      //   });
-      //})
+    })
+      //const path = svg.selectAll('path')
       .enter()
       .append('td')
+      //.append('path')
       .text(function (d) { return d.value; })
       .style("border-collapse", "collapse")
       .style("padding", "6px")
       .style("border", "1px lightgrey solid");
-  }
+
+  });
+
+  // function updateTable() {
+  //   // Set new data based on startPos and increment.
+  //   currentData = allData.slice(startPos, startPos + increment);
+
+  //   // Delete previous rows.
+  //   tbody.selectAll('tr').remove();
+
+  //   // Create new rows.
+  //   var tr = tbody.selectAll("tr")
+  //     .data(currentData).enter()
+  //     .append("tr")
+  //     .classed("even", function (d, i) {
+  //       return i % 2 == 1;
+  //     });
+
+  //   tr.selectAll('td')
+  //     .data(function (row) {
+  //       return columns.map(function (column) {
+  //         return { column: column, value: row[column] };
+  //       });
+  //     })
+  //     // .data(function (d) {
+  //     //   return keys.map(function (e) {
+  //     //     return {
+  //     //       key: e,
+  //     //       value: d[e]
+  //     //     }
+  //     //   });
+  //     //})
+  //     .enter()
+  //     .append('td')
+  //     .text(function (d) { return d.value; })
+  //     .style("border-collapse", "collapse")
+  //     .style("padding", "6px")
+  //     .style("border", "1px lightgrey solid");
+  // }
 }
     //return chart;
